07d8575c2b25b11fcb5d07882fdee3a46fa4baba
Adam Olah <olah_adam@yahoo.com>
2022-05-06 11:16:16 +0200

    init NavBarMain/Header/main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/NavBar_Main/Header';
+import Footer from './components/navbar/Footer';
+
+import Home from './components/main/Home';
+import About from './components/main/About';
+import Events from './components/main/Events';
 
 /* Layouts */
 import MainPageLayout from './layouts/MainPageLayout';
@@ -8,15 +15,23 @@ import UserMainPageLayout from './layouts/UserMainPageLayout';
 function App() {
     return (
         <div className='App'>
+
+            <Header />
             <Routes>
                 <Route element={<MainPageLayout />}>
-                    <Route path='/' element={<div>Main</div>} />
+                    <Route path='/' element={<Home />} />
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/events' element={<Events />} />
+                    <Route path='/login' element={<div>Login</div>} />
                 </Route>
 
                 <Route element={<UserMainPageLayout />}>
                     <Route path='/' element={<div>Profile</div>} />
                 </Route>
             </Routes>
+            <Footer />
+
         </div>
     );
 }
