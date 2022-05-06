d5242c6c981e54e4523530c15a35316f8add466b
Eszter Urban <eszter.urban0503@gmail.com>
2022-05-06 17:44:54 +0200

    route bővítés

--- src/components/navbar/UserNavbar.js
+++ src/components/navbar/UserNavbar.js
@@ -7,7 +7,7 @@ const UserNavbar = () =>{
             <li><NavLink to='/events'>Events</NavLink></li>
             <li><NavLink to='/addevent'>Create New Event</NavLink></li>
             <li><NavLink to='/myevents'>My Events</NavLink></li>
-            {<li><NavLink to='/chosenevents'>Chosen Events</NavLink></li>}
+            <li><NavLink to='/chosenevents'>Chosen Events</NavLink></li>
             <li><NavLink to='/searchevents'>Search Events</NavLink></li>
             <li><NavLink to='/settings'>Settings</NavLink></li>
             <li><NavLink to='signout'>Sign out</NavLink></li>
