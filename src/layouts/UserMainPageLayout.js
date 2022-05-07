45b0d6eb7bab9e359c9431d4d69b593e121de4c1
Eszter Urban <eszter.urban0503@gmail.com>
2022-05-07 10:37:03 +0200

    javítás

--- src/layouts/UserMainPageLayout.js
+++ src/layouts/UserMainPageLayout.js
@@ -1,7 +1,7 @@
 import {Outlet} from 'react-router-dom';
-import {UserNavbar} from '../components/Profile/UserNavbar';
-import {Header} from '../components/Header/Header';
-import {Footer} from '../components/Footer/Footer';
+import UserNavbar from '../components/Profile/UserNavbar';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 const UserMainPageLayout = () => {
   return (
