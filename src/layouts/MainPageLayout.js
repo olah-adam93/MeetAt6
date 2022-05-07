ea2228154db8be06ea98f754a788c1117a2f3409
Eszter Urban <eszter.urban0503@gmail.com>
2022-05-07 10:34:47 +0200

    layout kiegészítés

--- src/layouts/MainPageLayout.js
+++ src/layouts/MainPageLayout.js
@@ -1,11 +1,14 @@
-import { Outlet } from 'react-router-dom';
-
+import {Outlet} from 'react-router-dom';
+import {Header} from '../components/Header/Header';
+import {Footer} from '../components/Footer/Footer';
 const MainPageLayout = () => {
   return (
     <div>
       {/* header */}
+      <Header />
       <Outlet />
       {/* footer */}
+      <Footer />
     </div>
   );
 };
--- src/layouts/UserMainPageLayout.js
+++ src/layouts/UserMainPageLayout.js
@@ -1,7 +1,17 @@
-import { Outlet } from "react-router-dom";
+import {Outlet} from 'react-router-dom';
+import {UserNavbar} from '../components/Profile/UserNavbar';
+import {Header} from '../components/Header/Header';
+import {Footer} from '../components/Footer/Footer';
 
 const UserMainPageLayout = () => {
-  return <Outlet />;
+  return (
+    <div>
+      <Header />
+      <UserNavbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default UserMainPageLayout;
