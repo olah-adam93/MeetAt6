bf21b7b950e09b80d9ac376f933942004688e24c
szabolcs_derzsi <szabolcsnm@gmail.com>
2022-05-07 09:45:06 +0200

    add slider

--- src/views/HomePageView.js
+++ src/views/HomePageView.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import EventContainer from '../components/HomePage/EventContainer';
+import Slider from '../components/HomePage/Slider';
+
 export default function HomePageView() {
   return (
     <div>
       Home Page
+      <Slider />
       <EventContainer />
       {/*
       <Navbar />
