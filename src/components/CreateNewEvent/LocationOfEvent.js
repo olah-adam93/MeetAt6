2947d91c2d8648f8f76af5e5747ee3bea45ee39f
szabolcs_derzsi <szabolcsnm@gmail.com>
2022-05-08 15:54:09 +0200

    Merge branch 'main' into szabolcs_branch

--- src/components/CreateNewEvent/LocationOfEvent.js
+++ src/components/CreateNewEvent/LocationOfEvent.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+/* import {useState} from 'react'; */
 
 const LocationOfEvent = ({setLocationType, locationType}) => {
   const clickHandler= (e) => {
--- src/views/AddEventView.js
+++ src/views/AddEventView.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 /*Components */
 import BasicInfoForm from '../components/CreateNewEvent/BasicInfoForm';
 import LocationOfEvent from '../components/CreateNewEvent/LocationOfEvent';
