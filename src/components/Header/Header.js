28eac18e544a320a13ca657d846ccda9e392fa5d
szabolcs_derzsi <szabolcsnm@gmail.com>
2022-05-14 23:44:50 +0200

    header shrink

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   const [small, setSmall] = useState(false);
 
   useEffect(() => {
-    const scrollCallback = () => window.pageYOffset > 200 ? setSmall(true) : setSmall(false);
+    const scrollCallback = () => window.pageYOffset > 100 ? setSmall(true) : setSmall(false);
     if (typeof window !== 'undefined') {
       
       window.addEventListener('scroll', scrollCallback);
@@ -16,9 +16,9 @@ const Header = () => {
   }, []);
 
   return (
-    <div className='header-container'>
+    <div className={`header-container ${small ? 'header-small' : ''}`}>
       <div className='header-logo'>
-        <img src={require('./meetatsix200.49.feher.png')} alt='logo' />
+        <img className={`header-img ${small ? 'img-small' : ''}`} src={require('./meetatsix200.49.feher.png')} alt='logo' />
       </div>
       <div className='header-navbar'>
         <NavBarMain />
