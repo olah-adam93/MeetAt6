c3cef18168f68d2083e5c48d376e3e4fdc517fe5
iglodigergo <iglodigergo999@gmail.com>
2022-05-24 12:04:04 +0200

    delete events on profile

--- src/components/SearchEvent/DisplayItems.js
+++ src/components/SearchEvent/DisplayItems.js
@@ -1,14 +1,25 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 /* Style */
 import './Style/DisplayItems.css';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faAngleRight, faAngleLeft, faAnglesRight, faAnglesLeft} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faAngleRight,
+  faAngleLeft,
+  faAnglesRight,
+  faAnglesLeft,
+} from '@fortawesome/free-solid-svg-icons';
 
 /* Components */
 import EventCard from '../HomePage/EventCard';
 
-const DisplayItems = ({filteredDbItems, perPage, toDefault, setToDefault}) => {
+const DisplayItems = ({
+  isDeleteButton,
+  filteredDbItems,
+  perPage,
+  toDefault,
+  setToDefault,
+}) => {
   // console.log('DISPLAY ITEMS RENDER');
   const itemsPerPage = perPage;
   const [currentPage, setCurrentPage] = useState(1);
@@ -17,17 +28,25 @@ const DisplayItems = ({filteredDbItems, perPage, toDefault, setToDefault}) => {
   const totalItems = filteredDbItems.length;
   const pageNumber = Math.ceil(totalItems / itemsPerPage);
   const [itemsToRender, setItemsToRender] = useState([]);
-
+  const [modalOpen, setModalOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    if(toDefault) {
+    if (toDefault) {
       setCurrentPage(1);
       setfromIndex(0);
       setToIndex(itemsPerPage);
     }
-  }, [toDefault, itemsPerPage])
-
+  }, [toDefault, itemsPerPage]);
+  const deleteModalHandler = (e) => {
+    setModalOpen(!modalOpen);
+  };
+  const cancelHandler = () => {
+    setModalOpen(false);
+  };
+  const deleteHandler = (event) => {
+    console.log(event.target.parentElement);
+  };
 
   const nextButtonHandler = () => {
     setCurrentPage(currentPage + 1);
@@ -35,7 +54,7 @@ const DisplayItems = ({filteredDbItems, perPage, toDefault, setToDefault}) => {
     setToIndex(toIndex + itemsPerPage);
 
     setItemsToRender(filteredDbItems.slice(fromIndex, toIndex));
-    console.log("next page");
+    console.log('next page');
   };
 
   const previousButtonHandler = () => {
@@ -44,8 +63,8 @@ const DisplayItems = ({filteredDbItems, perPage, toDefault, setToDefault}) => {
     setToIndex(toIndex - itemsPerPage);
 
     setItemsToRender(filteredDbItems.slice(fromIndex, toIndex));
-    console.log("previous page");
-  }
+    console.log('previous page');
+  };
 
   const toFirstPageHandler = () => {
     setCurrentPage(1);
@@ -53,8 +72,8 @@ const DisplayItems = ({filteredDbItems, perPage, toDefault, setToDefault}) => {
     setToIndex(itemsPerPage);
 
     setItemsToRender(filteredDbItems.slice(fromIndex, toIndex));
-    console.log("to first page");
-  }
+    console.log('to first page');
+  };
 
   const toLastPageHandler = () => {
     setCurrentPage(pageNumber);
@@ -62,55 +81,111 @@ const DisplayItems = ({filteredDbItems, perPage, toDefault, setToDefault}) => {
     setToIndex(totalItems);
 
     setItemsToRender(filteredDbItems.slice(fromIndex, toIndex));
-    console.log("to last page");
-  }
+    console.log('to last page');
+  };
 
   useEffect(() => {
     // setLoading(true);
     setItemsToRender(filteredDbItems.slice(fromIndex, toIndex));
-    
+
     // setTimeout(() => {
     //   setItemsToRender(filteredDbItems.slice(fromIndex, toIndex));
     //   setLoading(false);
     // }, 500);
-    
-    setToDefault(false);
 
-  }, [filteredDbItems, fromIndex, toIndex, setToDefault])
+    setToDefault(false);
+  }, [filteredDbItems, fromIndex, toIndex, setToDefault]);
 
   return (
     <div className='display-items-container'>
       <div className='display-container'>
-        {!loading ? 
-          itemsToRender.map((event, index) => {
+        {modalOpen && (
+          <div>
+            <h2>Are you sure?</h2>
+            <button onClick={cancelHandler}>Cancel</button>
+            <button
+              onClick={(eventObj) => {
+                deleteHandler(eventObj);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        )}
+        {!loading ? (
+          itemsToRender.map((eventObj, index) => {
             return (
               <div key={index} className='display-items'>
                 <EventCard
-                eventCard={event[1]}
-                eventId={event[0]}
-                key={`card_${event[0]}`}
-              />
+                  eventCard={eventObj[1]}
+                  eventId={eventObj[0]}
+                  key={`card_${eventObj[0]}`}
+                />
+                {isDeleteButton && (
+                  <>
+                    <button type='button' onClick={deleteModalHandler}>
+                      Delete
+                    </button>
+                  </>
+                )}
               </div>
             );
-          }) : <div> Loading... </div>}
+          })
+        ) : (
+          <div> Loading... </div>
+        )}
       </div>
 
       <div className='pagination-container'>
-        {filteredDbItems.length > 0 ? <>
-        <button type='button' onClick={ toFirstPageHandler } disabled={currentPage > 2 ? '' : 'disabled'}><FontAwesomeIcon icon={faAnglesLeft} /></button>
-        <button type='button' onClick={ previousButtonHandler } disabled={currentPage === 1 ? 'disabled' : ''}><FontAwesomeIcon icon={faAngleLeft} /></button>
-        <span>{` Page ${currentPage} of ${pageNumber} `}</span>
-        <button type='button' onClick={ nextButtonHandler } disabled={currentPage === pageNumber ? 'disabled' : ''}><FontAwesomeIcon icon={faAngleRight} /></button>
-        <button type='button' onClick={ toLastPageHandler } disabled={currentPage < (pageNumber - 1) ? '' : 'disabled'}><FontAwesomeIcon icon={faAnglesRight} /></button>
-        </> :
-        <>
-        <button type='button' onClick={ toFirstPageHandler } disabled><FontAwesomeIcon icon={faAnglesLeft} /></button>
-        <button type='button' onClick={ previousButtonHandler } disabled><FontAwesomeIcon icon={faAngleLeft} /></button>
-        <span>{` Page ${currentPage} of ${1} `}</span>
-        <button type='button' onClick={ nextButtonHandler } disabled><FontAwesomeIcon icon={faAngleRight} /></button>
-        <button type='button' onClick={ toLastPageHandler } disabled><FontAwesomeIcon icon={faAnglesRight} /></button>
-        </>
-        }
+        {filteredDbItems.length > 0 ? (
+          <>
+            <button
+              type='button'
+              onClick={toFirstPageHandler}
+              disabled={currentPage > 2 ? '' : 'disabled'}
+            >
+              <FontAwesomeIcon icon={faAnglesLeft} />
+            </button>
+            <button
+              type='button'
+              onClick={previousButtonHandler}
+              disabled={currentPage === 1 ? 'disabled' : ''}
+            >
+              <FontAwesomeIcon icon={faAngleLeft} />
+            </button>
+            <span>{` Page ${currentPage} of ${pageNumber} `}</span>
+            <button
+              type='button'
+              onClick={nextButtonHandler}
+              disabled={currentPage === pageNumber ? 'disabled' : ''}
+            >
+              <FontAwesomeIcon icon={faAngleRight} />
+            </button>
+            <button
+              type='button'
+              onClick={toLastPageHandler}
+              disabled={currentPage < pageNumber - 1 ? '' : 'disabled'}
+            >
+              <FontAwesomeIcon icon={faAnglesRight} />
+            </button>
+          </>
+        ) : (
+          <>
+            <button type='button' onClick={toFirstPageHandler} disabled>
+              <FontAwesomeIcon icon={faAnglesLeft} />
+            </button>
+            <button type='button' onClick={previousButtonHandler} disabled>
+              <FontAwesomeIcon icon={faAngleLeft} />
+            </button>
+            <span>{` Page ${currentPage} of ${1} `}</span>
+            <button type='button' onClick={nextButtonHandler} disabled>
+              <FontAwesomeIcon icon={faAngleRight} />
+            </button>
+            <button type='button' onClick={toLastPageHandler} disabled>
+              <FontAwesomeIcon icon={faAnglesRight} />
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
--- src/views/MyEventsView.js
+++ src/views/MyEventsView.js
@@ -1,14 +1,14 @@
 import './Style/MyEventsView.css';
 
-import {useState, useContext, useEffect} from 'react';
+import { useState, useContext, useEffect } from 'react';
 import UserNavBar from '../components/Profile/UserNavbar';
 import DisplayItems from '../components/SearchEvent/DisplayItems';
 
 /* Database Context */
-import {EventDbContext} from '../components/EventDbContext/EventDbContext';
+import { EventDbContext } from '../components/EventDbContext/EventDbContext';
 
 /* AuthContext */
-import {getAuth} from '@firebase/auth';
+import { getAuth } from '@firebase/auth';
 // import { AuthContext } from '../components/Authentication/AuthContext';
 
 const MyEventsView = () => {
@@ -20,7 +20,6 @@ const MyEventsView = () => {
   const user = auth.currentUser;
 
   const [eventsCard, setEventsCard] = useState([]);
-
 
   useEffect(() => {
     console.log('eventdb: ', eventDb.db);
@@ -42,7 +41,16 @@ const MyEventsView = () => {
           <UserNavBar />
         </div>
         <div className='my-events-content'>
-          {eventsCard.length === 0 ? <div>No Events to display</div> : <DisplayItems filteredDbItems={eventsCard} perPage={3} setToDefault={() => {}}/>}
+          {eventsCard.length === 0 ? (
+            <div>No Events to display</div>
+          ) : (
+            <DisplayItems
+              filteredDbItems={eventsCard}
+              perPage={3}
+              setToDefault={() => {}}
+              isDeleteButton={true}
+            />
+          )}
         </div>
       </div>
     </>
