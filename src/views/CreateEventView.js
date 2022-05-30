50f3c3891b81f063710faf4dac2f3fcb6dc7d9e3
iglodigergo <iglodigergo999@gmail.com>
2022-05-30 18:54:22 +0200

    scroll when clicked on next button

--- src/views/CreateEventView.js
+++ src/views/CreateEventView.js
@@ -67,6 +67,12 @@ const CreateEventView = () => {
         setNextBtn((prev) => (prev += 1));
       }
     }
+
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
   };
   const backFormPageHandler = (e) => {
     e.preventDefault();
