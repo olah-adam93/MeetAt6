e35fe037449118177ff63225cb6f0d8b1b47f6ba
Adam Olah <olah_adam@yahoo.com>
2022-06-01 16:06:18 +0200

    sign-up/sign-in css

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -6,6 +6,14 @@ import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase
 import { auth } from '../../config/firebase';
 import { useNavigate } from 'react-router-dom';
 import { createUserData } from '../../services/crud';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPerson,
+  faEnvelope,
+  faLock,
+  faLocationDot,
+  faCircleExclamation,
+} from '@fortawesome/free-solid-svg-icons';
 
 import './SignUp.css';
 
@@ -30,52 +38,55 @@ const SignUp = ({ submitForm }) => {
         [e.target.name]: e.target.value,
       });
 
-      if (data?.nam && data?.e_mail && data?.passw && data?.passw.length > 5) {
+      /*if (data?.nam && data?.e_mail && data?.passw && data?.passw.length > 5) {
         setCorrectData(true);
-      }
+      } */
+      setError({});
+      setCorrectData(false);
     },
     [data]
   );
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    setError(Validation(data));
+    setError(validate(data));
 
-    console.log(correctData);
-    if (correctData) {
-      createUserWithEmailAndPassword(auth, data.e_mail, data.passw)
-        .then((authCredential) => {
-          // Sikeres volt a regisztracio
-          console.log('user', authCredential.user);
-          console.log('user', auth?.currentUser);
-        })
-        .then(() => {
-          /* const auth = getAuth(); */
-          const user = auth.currentUser;
-          if (user.uid) {
-            updateProfile(user, {
-              displayName: data.nam,
+    //console.log(correctData);
+    // if (correctData) {
+    createUserWithEmailAndPassword(auth, data.e_mail, data.passw)
+      .then((authCredential) => {
+        // Sikeres volt a regisztracio
+        console.log('user', authCredential.user);
+        console.log('user', auth?.currentUser);
+      })
+      .then(() => {
+        /* const auth = getAuth(); */
+        const user = auth.currentUser;
+        if (user.uid) {
+          updateProfile(user, {
+            displayName: data.nam,
+          })
+            .then(() => {
+              console.log(user.displayName);
             })
-              .then(() => {
-                console.log(user.displayName);
-              })
-              .catch((error) => {
-                console.log(error);
-              });
-            createUserData(`userDetails/${user.uid}`, {
-              location: data.loc,
-              organization: false,
+            .catch((error) => {
+              console.log(error);
             });
-          }
-        })
-        .then(() => {
-          /* const auth = getAuth(); */
-          const user = auth.currentUser;
-          authContext.setUserLog((prev) => ({ ...prev, user: user }));
-          navigateTo('/thankyou');
-        })
-        .catch((e) => console.log(e));
-    }
+          createUserData(`userDetails/${user.uid}`, {
+            location: data.loc,
+            organization: false,
+          });
+        }
+      })
+      .then(() => {
+        /* const auth = getAuth(); */
+        const user = auth.currentUser;
+        authContext.setUserLog((prev) => ({ ...prev, user: user }));
+        navigateTo('/thankyou');
+      })
+      .catch((e) => console.log(e));
+    //}
+    setCorrectData(true);
   };
 
   // useEffect(() => {
@@ -84,73 +95,150 @@ const SignUp = ({ submitForm }) => {
   //   }
   // }, [error, correctData, submitForm]);
 
+  // Validation
+
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+    if (!data.nam) {
+      errors.nam = 'Name cannot be blank!';
+    }
+    if (!data.e_mail) {
+      errors.e_mail = 'Email cannot be blank!';
+    } else if (!regex.test(data.e_mail)) {
+      errors.e_mail = 'This is not a valid email format!';
+    }
+    if (!data.passw) {
+      errors.passw = 'Password cannot be blank!';
+    } else if (data.passw.length < 5) {
+      errors.passw = 'Password must be more than 5 characters!';
+    } else if (data.passw.length > 10) {
+      errors.passw = 'Password cannot be more than 10 characters!';
+    }
+    if (!data.loc) {
+      errors.loc = 'Location cannot be blank!';
+    }
+    if (!data.organization) {
+      errors.organization = 'Please choose from the given options!';
+    }
+    return errors;
+  };
+
   return (
     <div className='sign-up-container'>
-      <div className='mainapp'></div>
-      <div>
-        <h2 className='title'> Sign Up </h2>
-      </div>
-      <form className='form-wrapper'>
-        <div className='name'>
-          <label className='label'>Name</label>
-          <input
-            className='input'
-            type='text'
-            name='nam'
-            value={data.nam}
-            onChange={handleChange}
-          />
-          {error.nam && <p className='errors'>{error.nam}</p>}
+      <form onSubmit={handleFormSubmit}>
+        <img
+          className='header-logo-img'
+          src={require('../../others/logo/meetatsix100.406.szines2.png')}
+          alt='logo'
+        />
+        <div className='sign-up-header'>
+          <h3> Sign Up </h3>
         </div>
-        <div className='mail'>
-          <label className='label'>Email</label>
-          <input
-            className='input'
-            type='email'
-            name='e_mail'
-            value={data.e_mail}
-            onChange={handleChange}
-          />
-          {error.e_mail && <p className='errors'>{error.e_mail}</p>}
+        {Object.keys(error).length === 0 && correctData ? (
+          <h4 className='signUp-success-msg'>Signed up successfully!</h4>
+        ) : null}
+        <div className={`name-container${error.nam ? ' shake' : ''}`}>
+          <label className='name-label'>Name</label>
+          <div className='name-field'>
+            <input
+              className='input'
+              type='text'
+              name='nam'
+              value={data.nam}
+              onChange={handleChange}
+            />
+            <FontAwesomeIcon className='icon-person' icon={faPerson} />
+            {error.nam ? (
+              <FontAwesomeIcon className='icon-error' icon={faCircleExclamation} />
+            ) : null}
+          </div>
+          <p className='error-msg'>{error.nam}</p>
         </div>
-        <div className='password'>
-          <label className='label'>Password</label>
-          <input
-            className='input'
-            type='password'
-            name='passw'
-            value={data.passw}
-            onChange={handleChange}
-          />
-          {error.passw && <p className='errors'>{error.passw}</p>}
+        <div className={`mail-container${error.e_mail ? ' shake' : ''}`}>
+          <label className='mail-label'>Email</label>
+          <div className='mail-field'>
+            <input
+              className='input'
+              type='text'
+              name='e_mail'
+              autoComplete='username'
+              value={data.e_mail}
+              onChange={handleChange}
+            />
+            <FontAwesomeIcon className='icon-envelope' icon={faEnvelope} />
+            {error.e_mail ? (
+              <FontAwesomeIcon className='icon-error' icon={faCircleExclamation} />
+            ) : null}
+          </div>
+          <p className='error-msg'>{error.e_mail}</p>
         </div>
-        <div className='location'>
-          <label className='label'>Location</label>
-          <input
-            className='input'
-            type='text'
-            name='loc'
-            value={data.loc}
-            onChange={handleChange}
-          />
-          {error.loc && <p className='errors'>{error.loc}</p>}
+        <div className={`password-container${error.passw ? ' shake' : ''}`}>
+          <label className='password-label'>Password</label>
+          <div className='password-field'>
+            <input
+              className='input'
+              type='password'
+              name='passw'
+              autoComplete='current-password'
+              value={data.passw}
+              onChange={handleChange}
+            />
+            <FontAwesomeIcon className='icon-lock' icon={faLock} />
+            {error.passw ? (
+              <FontAwesomeIcon className='icon-error' icon={faCircleExclamation} />
+            ) : null}
+          </div>
+          <p className='error-msg'>{error.passw}</p>
         </div>
-        <div className='organization'>
-          <label className='label'>Organization</label>
-          <input
-            className='input'
-            type='text'
-            name='organization'
-            value={data.organization}
-            onChange={handleChange}
-          />
-          {error.organization && <p className='errors'>{error.organization}</p>}
+        <div className={`location-signup-container${error.loc ? ' shake' : ''}`}>
+          <label className='location-label'>Location</label>
+          <div className='location-field'>
+            <input
+              className='input'
+              type='text'
+              name='loc'
+              value={data.loc}
+              onChange={handleChange}
+            />
+            <FontAwesomeIcon className='icon-location' icon={faLocationDot} />
+            {error.loc ? (
+              <FontAwesomeIcon className='icon-error' icon={faCircleExclamation} />
+            ) : null}
+            <p className='error-msg'>{error.loc}</p>
+          </div>
         </div>
-        <div>
-          <button className='submit' onClick={handleFormSubmit}>
-            Sign Up
-          </button>
+        <div className='classification-container'>
+          <div className='classification-field'>
+            <input
+              name='organization'
+              type='radio'
+              id='personal'
+              value={false}
+              onChange={handleChange}
+            />
+            <label className='classification-label' htmlFor='personal'>
+              Personal
+            </label>
+          </div>
+          <div className='classification-field'>
+            <input
+              name='organization'
+              type='radio'
+              id='organization'
+              value={true}
+              onChange={handleChange}
+            />
+            <label className='classification-label' htmlFor='organization'>
+              Organization
+            </label>
+          </div>
         </div>
+        <div className='classification-msg'>
+          <p className='error-msg'>{error.organization}</p>
+        </div>
+        <button className='submit'>Sign Up</button>
       </form>
     </div>
   );
