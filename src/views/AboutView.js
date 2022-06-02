6464cc1ebc3046af9d9a2dc3c622e33c1a31e38b
Raven <holloprof@gmail.com>
2022-06-02 19:18:08 +0200

    About us css patch

--- src/views/AboutView.js
+++ src/views/AboutView.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import './Style/AboutView.css';
+import about1 from '../others/decoration/about1.svg'
+import about2 from '../others/decoration/about2.svg'
+import about3 from '../others/decoration/about3.svg'
+import about4 from '../others/decoration/about4.svg'
 
 const AboutView = () => {
   return <div className='about-container'>
     <div className="about-image-left-container">
-       <img className="about-image-left-one" src="https://i1.wp.com/www.burning-glass.com/wp-content/uploads/coding_400x267-1.jpg?fit=400%2C267&ssl=1"></img>
-       <img className="about-image-left-two" src="https://www.openaccessgovernment.org/wp-content/uploads/2021/09/dreamstime_xl_132748845-scaled.jpg"></img>
+       <img className="about-image-left-one" src={about1}></img>
+       <img className="about-image-left-two" src={about2}></img>
     </div>
       
     
     <div className="about-text-container">
-    <h3 className='about-title'>Who are we?</h3>
-    <div className='about-body'>Once upon a time, there were six young people on the planet earth. One day, these six, different person decided to sign up 
-    to the course of the Progmatic School. After a long period, they learnt the basics of frontend programming, or something more. At the and of the course, they had to
-    create their frist project, as some kind of final exam. Their goal was to create an online page, were people could browse between events, in various theme. The time
-    was shot, they worked a lot, but in the end, the project coming alive. Or at least they hoped, becouse when I wrote this we were only in the middle of the progress.
-    But be positive, we will prevail. </div>
+    <h1 className='about-title'>Who are we?</h1>
+    <div className='about-body'>Once upon a time, there were six young people on the planet Earth. One day, these six, different person decided to sign up 
+    to the course of the Progmatic School. After a long period, they learnt the basics of the frontend programming, met a the way of the coding or something more. At the end of 
+    the course, they had to create their frist project, as some kind of final exam. Their goal was to create an online page, were people could browse between events, in 
+    various theme. Just a little help for those who want to join communities, or just want to spend somehow their free tirm. The time was short, they worked a lot, but 
+    in the end, the project coming alive. Or at least they hoped, becouse when I wrote this we were only in the middle of the progress. But be positive, we will prevail. </div>
     </div>;
 
     <div className="about-image-right-container">
-        <img className="about-image-right-one" src="https://cdn.mos.cms.futurecdn.net/sdSHp2akMYc4EoZAoRE77k-1200-80.jpg"></img>
-        <img className="about-image-right-two" src="https://www.bloemfonteincourant.co.za/wp-content/uploads/2018/07/digital-coding-785.jpg"></img>
+        <img className="about-image-right-one" src={about3}></img>
+        <img className="about-image-right-two" src={about4}></img>
     </div>
     </div>
 
