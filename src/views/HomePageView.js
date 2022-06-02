5a4fafc224064289cc1836f45f881e6c5527c8e9
szakos96 <szokeakos96@gmail.com>
2022-06-02 19:56:06 +0200

    Pictures details

--- src/views/HomePageView.js
+++ src/views/HomePageView.js
@@ -4,12 +4,14 @@ import EventSlider from '../components/HomePage/EventSlider';
 
 /* Style */
 import './Style/HomePageView.css';
+import PicturesDetails from '../components/HomePage/PicturesDetails';
 
 
 const HomePageView = () => {
   return (
     <div>
       <PictureSlider />
+      <PicturesDetails />
       {/* <div className='separator'></div> */}
       <EventSlider
         containerName={'Newest events'}
